62e9e1955b1bda09b5d7be7b76856f07647dfbf5
tungUAS <Tung.Trinh@senacor.com>
2023-09-18 23:16:31 +0200

    make name unique bucket

--- resource.tf
+++ resource.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "test-bucket-abcxzy-tung" {
-  name          = "test-bucket"
+  name          = "test-bucket-abcxzy-tung"
   location      = "EU"
   storage_class = "STANDARD"
   project       = var.project_id
