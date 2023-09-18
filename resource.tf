d59484f4722444dbd94145dd787055b4ad9a3b4b
tungUAS <Tung.Trinh@senacor.com>
2023-09-18 23:59:37 +0200

    add new vpc

--- resource.tf
+++ resource.tf
@@ -4,3 +4,10 @@ resource "google_storage_bucket" "test-bucket-abcxzy-tung" {
   storage_class = "STANDARD"
   project       = var.project_id
 }
+
+resource "google_compute_network" "vpc_network" {
+  project                 = var.project_id
+  name                    = "vpc-sql-instance"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
