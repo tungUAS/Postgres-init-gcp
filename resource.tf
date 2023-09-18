2b80c592cab3d5348678d10d41c57387daf03d55
tungUAS <Tung.Trinh@senacor.com>
2023-09-19 00:36:13 +0200

    add sql instance

--- resource.tf
+++ resource.tf
@@ -4,3 +4,22 @@ resource "google_storage_bucket" "test-bucket-abcxzy-tung" {
   storage_class = "STANDARD"
   project       = var.project_id
 }
+
+resource "google_sql_database_instance" "sql-instance-abcxzy-tung" {
+  name             = "sql-instance-abcxzy-tung"
+  region           = "europe-west4"
+  database_version = "POSTGRES_14"
+  root_password    = "abcABC123!"
+  settings {
+    tier = "db-custom-2-7680"
+    password_validation_policy {
+      min_length                  = 6
+      reuse_interval              = 2
+      complexity                  = "COMPLEXITY_DEFAULT"
+      disallow_username_substring = true
+      password_change_interval    = "30s"
+      enable_password_policy      = true
+    }
+  }
+  deletion_protection = false
+}
