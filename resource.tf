afbf4777f96b6927261e5268360a24d1b7e33ba1
tungUAS <Tung.Trinh@senacor.com>
2023-09-19 00:31:33 +0200

    remove unused resource

--- resource.tf
+++ resource.tf
@@ -4,10 +4,3 @@ resource "google_storage_bucket" "test-bucket-abcxzy-tung" {
   storage_class = "STANDARD"
   project       = var.project_id
 }
-
-resource "google_compute_network" "vpc_network" {
-  project                 = var.project_id
-  name                    = "vpc-sql-instance"
-  auto_create_subnetworks = false
-  mtu                     = 1460
-}
