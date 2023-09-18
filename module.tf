4767a7ef3727437ffefcbb959d9ffa1a8cf970f6
tungUAS <Tung.Trinh@senacor.com>
2023-09-19 00:27:37 +0200

    init vpc subnets

---
+++ module.tf
@@ -0,0 +1,20 @@
+module "test-vpc-module" {
+  source       = "terraform-google-modules/network/google"
+  version      = "~> 7.0"
+  project_id   = var.project_id
+  network_name = "vpc-sql-instance"
+  mtu          = 1460
+
+  subnets = [
+    {
+      subnet_name   = "subnet-01"
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = var.region
+    },
+    {
+      subnet_name   = "subnet-02"
+      subnet_ip     = "10.10.20.0/24"
+      subnet_region = var.region
+    },
+  ]
+}
