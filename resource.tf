dabfffcfe166028e833d1b991b85cc066ab674c4
tungUAS <Tung.Trinh@senacor.com>
2023-09-19 15:14:54 +0200

    add dummy instance

--- resource.tf
+++ resource.tf
@@ -24,6 +24,25 @@ resource "google_sql_database_instance" "sql-instance-abcxzy-tung" {
   deletion_protection = false
 }
 
+resource "google_sql_database_instance" "sql-instance-tung" {
+  name             = "sql-instance-tung"
+  region           = "europe-west4"
+  database_version = "POSTGRES_14"
+  root_password    = "abcABC123!"
+  settings {
+    tier = "db-custom-2-7680"
+    password_validation_policy {
+      min_length                  = 6
+      reuse_interval              = 2
+      complexity                  = "COMPLEXITY_DEFAULT"
+      disallow_username_substring = true
+      password_change_interval    = "30s"
+      enable_password_policy      = true
+    }
+  }
+  deletion_protection = false
+}
+
 
 resource "google_sql_database_instance" "sql-instance-psc-tung" {
   name             = "sql-instance-psc-tung"
