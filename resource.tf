f82594368ba85af9e8cf2ddd49768f4a53f284d5
tungUAS <Tung.Trinh@senacor.com>
2023-09-19 15:12:07 +0200

    add region

--- resource.tf
+++ resource.tf
@@ -27,6 +27,7 @@ resource "google_sql_database_instance" "sql-instance-abcxzy-tung" {
 
 resource "google_sql_database_instance" "sql-instance-psc-tung" {
   name             = "sql-instance-psc-tung"
+  region           = "europe-west4"
   database_version = "POSTGRES_14"
   settings {
     tier = "db-custom-2-7680"
