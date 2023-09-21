e0f01dadb31ae08b88516e17b24ddaa46813adc6
tungUAS <Tung.Trinh@senacor.com>
2023-09-21 14:56:36 +0200

    add vm for psa vpc

--- resource.tf
+++ resource.tf
@@ -57,3 +57,68 @@ resource "google_sql_database_instance" "sql-instance-psa" {
   }
   deletion_protection = false
 }
+
+# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
+# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+resource "google_compute_instance" "instance-vm-sql-psa" {
+  boot_disk {
+    auto_delete = true
+    device_name = "instance-vm-sql-psa"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-11-bullseye-v20230912"
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-micro"
+  name         = "instance-vm-sql-psa"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    network = "projects/cst-pbag-aiml-test/global/networks/default"
+  }
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    subnetwork = "projects/cst-pbag-aiml-test/regions/europe-west4/subnetworks/subnet-vpc-sql-psa"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "507085571061-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = "europe-west4-a"
+}
