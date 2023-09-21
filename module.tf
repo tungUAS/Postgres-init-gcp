55658f1a7e79da016bbf3c4c77eb8d2dd02d561b
tungUAS <Tung.Trinh@senacor.com>
2023-09-21 19:17:30 +0200

    add vpc network for psc connection

--- module.tf
+++ module.tf
@@ -36,3 +36,20 @@ module "vpc-sql-module" {
     },
   ]
 }
+
+module "vpc-sql-module" {
+  source                  = "terraform-google-modules/network/google"
+  version                 = "~> 7.0"
+  project_id              = var.project_id
+  network_name            = "vpc-sql-psc-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+
+  subnets = [
+    {
+      subnet_name   = "subnet-vpc-sql-psc"
+      subnet_ip     = "10.10.40.0/24"
+      subnet_region = var.region
+    },
+  ]
+}
